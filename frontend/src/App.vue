6a7206f3df729c93a151cc05e398257de2151258
sumankr0087 <suman.kumar@essentia.dev>
2024-06-26 06:35:19 +0530

    initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text-3xl text-red-600">dkjlfndsldolsdlsd</div>
+</template>
+
+<script>
+import './style.css'
+
+export default {
+  name: 'App',
+  components: { }
+}
+</script>
+
+<style>
+
+</style>
